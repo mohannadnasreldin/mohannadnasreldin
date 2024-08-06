a5dd8d1770f3a3f2f8f01bc4f22fc5681909a0bb
Mohannad Nasreldin Zaghloul <mohannadnasr.20@gmail.com>
2024-08-06 12:38:35 +0300

    editing cv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Skills from './components/Skills';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Experience from './components/Experience';
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Experience from "./components/Experience";
+
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className={`flex flex-col min-h-screen`}>
       <Header />
       <main className="flex-grow">
-        <Hero id="home" />       {/* Ensure this has id="home" */}
-        <Services id="services" /> {/* Ensure this has id="services" */}
-        <Experience id="experience"/>
-        <Skills id="skills" />    {/* Ensure this has id="skills" */}
-        <About id="about" />      {/* Ensure this has id="about" */}
-        <Contact id="contact" />  {/* Ensure this has id="contact" */}
+        <Hero id="home" />
+        <Services id="services" />
+        <Experience id="experience" />
+        <Skills id="skills" />
+        <About id="about" />
+        <Contact id="contact" />
       </main>
       <Footer />
     </div>
